4f22e506adfaba0325e480ef236591d62e3bfcc3
Ilya Utkin <ilyautkin@mail.ru>
2013-03-05 18:04:13 +0400

    Опять правил файл сборки пакета

--- _build/build.config.php
+++ _build/build.config.php
@@ -1,9 +1,10 @@
 <?php
-
-error_reporting(E_ALL ^E_NOTICE);
-ini_set('display_errors', true);
- 
- 
+/**
+ * Define the MODX path constants necessary for installation
+ *
+ * @package themebootstrap
+ * @subpackage build
+ */
 define('MODX_BASE_PATH', dirname(dirname(dirname(__FILE__))) . '/');
 define('MODX_CORE_PATH', MODX_BASE_PATH . 'core/');
 define('MODX_MANAGER_PATH', MODX_BASE_PATH . 'manager/');
@@ -15,40 +16,3 @@ define('MODX_CORE_URL', MODX_BASE_URL . 'core/');
 define('MODX_MANAGER_URL', MODX_BASE_URL . 'manager/');
 define('MODX_CONNECTORS_URL', MODX_BASE_URL . 'connectors/');
 define('MODX_ASSETS_URL', MODX_BASE_URL . 'assets/');
- 
- 
- 
-/*
- * Include MODX config
- */
-require_once MODX_CORE_PATH . 'config/config.inc.php';
-
-/* define sources */
-$root = MODX_BASE_PATH; 
-
-/*
- * Константы
- */
-$sources = array(
-    'root' => $root,
-    'build' => $root . '_build/',
-    'data' => $root . '_build/data/',
-    'resolvers' => $root . '_build/resolvers/',
-    'chunks' => $root.'core/components/'.PKG_PATH.'/elements/chunks/',
-    'snippets' => $root.'core/components/'.PKG_PATH.'/elements/snippets/',
-    'plugins' => $root.'core/components/'.PKG_PATH.'/elements/plugins/',
-    'lexicon' => $root . 'core/components/'.PKG_PATH.'/lexicon/',
-    'docs' => $root.'core/components/'.PKG_PATH.'/docs/',
-    'pages' => $root.'core/components/'.PKG_PATH.'/elements/pages/',
-    'source_assets' => $root.'assets/components/'.PKG_PATH,
-    'source_core' => $root.'core/components/'.PKG_PATH,
-    'templates' => $root.'core/components/'.PKG_PATH.'/elements/templates/',
-    'model' => $root.'core/components/'.PKG_PATH.'/model/',
-);
-unset($root);
- 
-require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
-require_once $sources['build'] . 'includes/functions.php';
-
-$modx= new modX();
-$modx->initialize('mgr'); 
--- _build/build.transport.php
+++ _build/build.transport.php
@@ -1,79 +1,90 @@
 <?php
 
-$pkg_name = 'editLinksNet';
-    
-/* define package */
-define('PKG_NAME', $pkg_name);
-define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('NAMESPACE_NAME', PKG_NAME_LOWER);
-
-define('PKG_PATH', PKG_NAME_LOWER);
-define('PKG_CATEGORY', PKG_NAME);
-
-$pkg_version = '0.1.0';
-$pkg_release = 'beta';
-define('PKG_VERSION', $pkg_version); 
-define('PKG_RELEASE', $pkg_release); 
-
-
-$mtime= microtime();
-$mtime= explode(" ", $mtime);
-$mtime= $mtime[1] + $mtime[0];
+$mtime = microtime();
+$mtime = explode(' ', $mtime);
+$mtime = $mtime[1] + $mtime[0];
 $tstart = $mtime;
+set_time_limit(0);
 
+/* define package */
+define('PKG_NAME','editLinksNet');
+define('PKG_NAME_LOWER','editlinksnet');
+define('PKG_VERSION','0.1.1');
+define('PKG_RELEASE','pl');
 
+/* define sources */
+$root = dirname(dirname(__FILE__)).'/';
+$sources = array(
+    'root' => $root,
+    'build' => $root . '_build/',
+    'data' => $root . '_build/data/',
+    'chunks' => $root.'assets/components/'.PKG_NAME_LOWER.'/elements/chunks/',
+    'templates' => $root.'assets/components/'.PKG_NAME_LOWER.'/elements/templates/',
+    'plugins' => $root.'assets/components/'.PKG_NAME_LOWER.'/elements/plugins/',
+    'docs' => $root.'assets/components/'.PKG_NAME_LOWER.'/docs/',
+    'source_assets' => $root.'assets/components/'.PKG_NAME_LOWER,
+);
+unset($root);
 
-//print '<pre>';
-require_once dirname(__FILE__). '/build.config.php'; 
-/*
- * Set log Params
- */
+/* override with your own defines here (see build.config.sample.php) */
+require_once $sources['build'] . '/build.config.php';
+require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+require_once $sources['build'] . '/includes/functions.php';
+
+$modx= new modX();
+$modx->initialize('mgr');
+echo '<pre>'; /* used for nice formatting of log messages */
 $modx->setLogLevel(modX::LOG_LEVEL_INFO);
-$modx->setLogTarget('ECHO'); /*echo '<pre>';*/ flush();
+$modx->setLogTarget('ECHO');
 
 $modx->loadClass('transport.modPackageBuilder','',false, true);
-
-
-/*
- * Create builder
- */
 $builder = new modPackageBuilder($modx);
-
-/*
- * create Package
- */
-$builder->createPackage(PKG_NAME,PKG_VERSION,PKG_RELEASE);
+$builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
 $builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
-
-/*
- * load lexicon
- */
-/*$modx->getService('lexicon','modLexicon');
-$modx->lexicon->load(PKG_NAME_LOWER.':properties');*/
- 
-
-
-/* add namespace */
-$namespace = $modx->newObject('modNamespace');
-$namespace->set('name', NAMESPACE_NAME);
-$namespace->set('path',"{core_path}components/".PKG_NAME_LOWER."/");
-$namespace->set('assets_path',"{assets_path}components/".PKG_NAME_LOWER."/");
-$vehicle = $builder->createVehicle($namespace,array(
-    xPDOTransport::UNIQUE_KEY => 'name',
-    xPDOTransport::PRESERVE_KEYS => true,
-    xPDOTransport::UPDATE_OBJECT => true,
-));
-$builder->putVehicle($vehicle);
-$modx->log(modX::LOG_LEVEL_INFO,"Packaged in ".NAMESPACE_NAME." namespace."); flush();
-unset($vehicle,$namespace);
-
+$modx->log(modX::LOG_LEVEL_INFO,'Created Transport Package and Namespace.');
 
 /* create category */
 $category= $modx->newObject('modCategory');
 $category->set('id',1);
 $category->set('category',PKG_NAME);
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in category.'); flush();
-  
+
+/* add templates */
+/*
+$templates = include $sources['data'].'transport.templates.php';
+if (!is_array($templates)) {
+    $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in templates.');
+} else {
+    $category->addMany($templates);
+    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($templates).' templates.');
+}
+*/
+
+/* add chunks */
+$chunks = include $sources['data'].'transport.chunks.php';
+if (!is_array($chunks)) {
+    $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in chunks.');
+} else {
+    $category->addMany($chunks);
+    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($chunks).' chunks.');
+}
+
+/* add plugins */ 
+$plugins = include $sources['data'].'transport.plugins.php';
+if (!is_array($plugins)) {
+	$modx->log(modX::LOG_LEVEL_ERROR,'Adding plugins failed.'); } 
+else{
+    $category->addMany($plugins);
+    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($plugins).' plugins.');
+}
+
+/* add snippets */
+$snippets = include $sources['data'].'transport.snippets.php';
+if (!is_array($snippets)) {
+    $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in snippets.');
+} else {
+    $category->addMany($snippets);
+    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($snippets).' snippets.');
+}
 
 /* create category vehicle */
 $attr = array(
@@ -82,14 +93,42 @@ $attr = array(
     xPDOTransport::UPDATE_OBJECT => true,
     xPDOTransport::RELATED_OBJECTS => true,
     xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-        'Snippets' => array(
+        'Children' => array(
             xPDOTransport::PRESERVE_KEYS => false,
             xPDOTransport::UPDATE_OBJECT => true,
-            xPDOTransport::UNIQUE_KEY => 'name',
+            xPDOTransport::UNIQUE_KEY => 'category',
+            xPDOTransport::RELATED_OBJECTS => true,
+            xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
+                'Templates' => array(
+                    xPDOTransport::PRESERVE_KEYS => false,
+                    xPDOTransport::UPDATE_OBJECT => false,
+                    xPDOTransport::UNIQUE_KEY => 'templatename',
+                ),
+                'Chunks' => array(
+                    xPDOTransport::PRESERVE_KEYS => false,
+                    xPDOTransport::UPDATE_OBJECT => false,
+                    xPDOTransport::UNIQUE_KEY => 'name',
+                ),
+				'Plugins' => array(
+					xPDOTransport::PRESERVE_KEYS => false,
+					xPDOTransport::UPDATE_OBJECT => true,
+					xPDOTransport::UNIQUE_KEY => 'name',
+				),
+				'PluginEvents' => array(
+					xPDOTransport::PRESERVE_KEYS => true,
+					xPDOTransport::UPDATE_OBJECT => false,
+					xPDOTransport::UNIQUE_KEY => array('pluginid','event'),
+				),
+            ),
         ),
-        'Chunks' => array(
+        'Templates' => array(
             xPDOTransport::PRESERVE_KEYS => false,
-            xPDOTransport::UPDATE_OBJECT => true,
+            xPDOTransport::UPDATE_OBJECT => false,
+            xPDOTransport::UNIQUE_KEY => 'templatename',
+        ),
+        'Chunks' => array (
+            xPDOTransport::PRESERVE_KEYS => false,
+            xPDOTransport::UPDATE_OBJECT => false,
             xPDOTransport::UNIQUE_KEY => 'name',
         ),
         'Plugins' => array(
@@ -102,75 +141,30 @@ $attr = array(
             xPDOTransport::UPDATE_OBJECT => false,
             xPDOTransport::UNIQUE_KEY => array('pluginid','event'),
         ),
-    )
+    ),
 );
-
-
-/* add plugins */ 
-$plugins = include $sources['data'].'transport.plugins.php';
-
-if (!is_array($plugins)) { $modx->log(modX::LOG_LEVEL_FATAL,'Adding plugins failed.'); } 
-else{
-    $category->addMany($plugins);
-    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($plugins).' plugins.'); flush();
-}
-
-unset($plugins,$plugin,$attributes);
-
-
-/* add snippets */
-$snippets = include $sources['data'].'transport.snippets.php';
-if (!is_array($snippets)) {
-    $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in snippets.');
-} else {
-    $category->addMany($snippets);
-    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($snippets).' snippets.');
-}
-
-/* add chunks */
-$chunks = include $sources['data'].'transport.chunks.php';
-if (!is_array($chunks)) {
-    $modx->log(modX::LOG_LEVEL_ERROR,'Could not package in chunks.');
-} else {
-    $category->addMany($chunks);
-    $modx->log(modX::LOG_LEVEL_INFO,'Packaged in '.count($chunks).' chunks.');
-}
-
-/*
- * Create category vehicle
- */
 $vehicle = $builder->createVehicle($category,$attr);
 
-// Add core source
-$vehicle->resolve('file',array(
-    'source' => $sources['source_core'],
-    'target' => "return MODX_CORE_PATH . 'components/';",
-));
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in CorePath'); flush();
-
-// Add assets source
+$modx->log(modX::LOG_LEVEL_INFO,'Adding file resolvers to category...');
 $vehicle->resolve('file',array(
     'source' => $sources['source_assets'],
     'target' => "return MODX_ASSETS_PATH . 'components/';",
 ));
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in AssetsPath'); flush();
- 
-
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in resolvers.'); 
-
-flush();
-
 $builder->putVehicle($vehicle);
 
 /* now pack in the license file, readme and setup options */
 $builder->setPackageAttributes(array(
+    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
     'license' => file_get_contents($sources['docs'] . 'license.txt'),
     'readme' => file_get_contents($sources['docs'] . 'readme.txt'),
-    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'), 
+    //'setup-options' => array(
+        //'source' => $sources['build'].'setup.options.php',
+    //),
 ));
-$modx->log(modX::LOG_LEVEL_INFO,'Packaged in package attributes.'); flush();
+$modx->log(modX::LOG_LEVEL_INFO,'Added package attributes and setup options.');
 
-$modx->log(modX::LOG_LEVEL_INFO,'Packing...'); flush();
+/* zip up package */
+$modx->log(modX::LOG_LEVEL_INFO,'Packing up transport package zip...');
 $builder->pack();
 
 $mtime= microtime();
@@ -183,5 +177,3 @@ $totalTime= sprintf("%2.4f s", $totalTime);
 $modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
 
 exit ();
-
-?>
